b3b4b2abc66ee6c3e8218ad50186d1c3a407215a
Andy Tomaszewski <zueski@gmail.com>
2011-08-29 23:33:52 -0400

    Missed a couple files to add

---
+++ src/house/neko/media/device/Device.java
@@ -0,0 +1,20 @@
+package house.neko.media.device;
+
+import house.neko.media.common.Media;
+import house.neko.media.common.MediaLocation;
+
+import java.io.File;
+import java.io.IOException;
+
+public interface Device
+{
+	public File copyTo(Media m) throws Exception;
+	
+	public File getDeviceFile(Media m, MediaLocation l) throws IOException;
+	
+	public void deleteAll() throws IOException;
+	
+	public long getFreeSpace();
+	
+	public long getAvailableSpaceForTracks();
+}
