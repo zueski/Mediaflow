90c89d72c1051526b7c94a56e0c12f993a71e3a3
Andy Tomaszewski <zueski@gmail.com>
2011-09-18 12:27:45 -0400

    Finished transcoding class, still need to test

--- src/house/neko/media/device/MP3Converter.java
+++ src/house/neko/media/device/MP3Converter.java
@@ -29,6 +29,7 @@ public class MP3Converter //implements Converter
 	static public boolean writeToFile(Media m, MediaLocation l, File outFile) 
 	{
 		setupDefaults();
+		AudioInputStream inFileAIS = null;
 		try 
 		{
 			InputStream inFile = l.getInputStream();
@@ -39,31 +40,82 @@ public class MP3Converter //implements Converter
 			if(inFileFormat.getType() != mp3Format.getType()) 
 			{
 				// inFile is not AIFF, so let's try to convert it.
-				AudioInputStream inFileAIS = AudioSystem.getAudioInputStream(inFile);
+				inFileAIS = AudioSystem.getAudioInputStream(inFile);
 				//inFileAIS.reset(); // rewind
-				if(AudioSystem.isFileTypeSupported(mp3Format.getType(), inFileAIS)) 
+				if(!AudioSystem.isFileTypeSupported(mp3Format.getType(), inFileAIS)) 
 				{
-					AudioSystem.write(inFileAIS, mp3Format.getType(), outFile);
-					System.out.println("Successfully made " + mp3Format.getType() + " file, " + outFile.getAbsolutePath() + ", from " + inFileFormat.getType() + " file");
-					inFileAIS.close();  // All done now
-				} else { 
 					System.out.println("Warning: " + mp3Format.getType() + " conversion of "  + inFileFormat.getType() + " is not currently supported by AudioSystem.");
-					return false;
+					inFileAIS = getStreamAsPCM(inFileAIS);
+					if(inFileAIS == null)
+					{
+						System.out.println("Unable to get AIFF stream from input");
+						return false;
+					}
 				}
+				AudioSystem.write(inFileAIS, mp3Format.getType(), outFile);
+				System.out.println("Successfully made " + mp3Format.getType() + " file, " + outFile.getAbsolutePath() + ", from " + inFileFormat.getType() + " file");
+				inFileAIS.close();  // All done now
 			} else {
 				System.out.println("Input file " + inFileFormat.getType() + " is MP3. Conversion is unnecessary.");
 			}
-		} catch (UnsupportedAudioFileException e) {
-			System.err.println("Error: " + m + " is not a supported audio file type: " + e);
+		} catch(UnsupportedAudioFileException e) {
+			System.err.println("Error: " + m + " is not a supported audio file type from " + inFileAIS.getFormat() + ": " + e);
 			e.printStackTrace();
 			return false;
-		} catch (IOException e) {
+		} catch(IOException e) {
 			System.err.println("Error: failure attempting to read "  + l.getLocationURLString() + ": " + e);
+			e.printStackTrace();
+			return false;
+		} catch(IllegalArgumentException e) {
+			System.err.println("Error: MP3 is not a supported audio file type from " + inFileAIS.getFormat() + ": " + e);
 			e.printStackTrace();
 			return false;
 		}
 		return true;
 	}
+	
+	static public AudioInputStream getInputStream(Media m, MediaLocation l) 
+	{
+		setupDefaults();
+		AudioInputStream inFileAIS = null;
+		try 
+		{
+			InputStream inFile = l.getInputStream();
+			// query file type
+			AudioFileFormat inFileFormat = AudioSystem.getAudioFileFormat(inFile);
+			inFile.reset(); // rewind
+			AudioFileFormat mp3Format = new javazoom.spi.mpeg.sampled.file.MpegAudioFileFormat(type, audioFormat, nLengthInFrames, nLengthInBytes, properties);
+			if(inFileFormat.getType() != mp3Format.getType()) 
+			{
+				// inFile is not AIFF, so let's try to convert it.
+				inFileAIS = AudioSystem.getAudioInputStream(inFile);
+				//inFileAIS.reset(); // rewind
+				if(!AudioSystem.isFileTypeSupported(mp3Format.getType(), inFileAIS)) 
+				{
+					System.out.println("Warning: " + mp3Format.getType() + " conversion of "  + inFileFormat.getType() + " is not currently supported by AudioSystem.");
+					inFileAIS = getStreamAsPCM(inFileAIS);
+					if(inFileAIS == null)
+					{	System.out.println("Unable to get AIFF stream from input"); }
+				}
+			} else {
+				System.out.println("Input file " + inFileFormat.getType() + " is MP3. Conversion is unnecessary.");
+			}
+		} catch(UnsupportedAudioFileException e) {
+			System.err.println("Error: " + m + " is not a supported audio file type from " + inFileAIS.getFormat() + ": " + e);
+			e.printStackTrace();
+		} catch(IOException e) {
+			System.err.println("Error: failure attempting to read "  + l.getLocationURLString() + ": " + e);
+			e.printStackTrace();
+		} catch(IllegalArgumentException e) {
+			System.err.println("Error: MP3 is not a supported audio file type from " + inFileAIS.getFormat() + ": " + e);
+			e.printStackTrace();
+		}
+		return inFileAIS;
+	}
+	
+	static private AudioInputStream getStreamAsPCM(AudioInputStream is)
+		throws IllegalArgumentException
+	{	return AudioSystem.getAudioInputStream(AudioFormat.Encoding.PCM_SIGNED, is); }
 	
 	static public void main(String[] args)
 		throws Exception
